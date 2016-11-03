3e46e30bac73f2450a064f8fd1d79a0e2d2e8a37
鲁法海 <lu.fahai@trs.com.cn>
2016-11-03 15:08:56 +0800

    添加css样式，修改js

--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -10,6 +10,7 @@
 <html>
 <head>
     <title>拓尔思短网址</title>
+    <link rel="stylesheet" type="text/css" href="../../resources/css/index.css">
     <script type="text/javascript" src="../../resources/js/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="../../resources/js/index.js"></script>
 </head>
@@ -17,14 +18,11 @@
 <form id="form" action="/shortURL/save" method="post">
     <div id="urlDiv">
     <input id="longURL" type="text" name="longURL"/>
-        <input type="button" value="判断是否可访问" onclick="getURL()"/>
-    <input id="button" type="submit" value="生成短网址"/>
+    <input id="button" type="button" value="生成短网址"/>
     </div>
 </form>
-<div id="msg1"></div>
-<div id="msg"></div>
 <c:if  test="${!empty shortURL}">
-    <label id="shortURL">${shortURL}</label>
+    <textarea id="shortURL" cols="44" rows="1">${shortURL}</textarea>
     <input type="button" value="复制" onclick="copy()">
 </c:if>
 </body>
--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,66 +1,31 @@
+jQuery(document).ready(function ($) {
+    jQuery("#button").click(function(){
+        var url = $("#longURL").val();//请求的url
+        if (!(url.indexOf("http://") == 0 || url.indexOf("https://") == 0)){
+            url = "http://" + url;
+        }
+        jQuery.ajax({
+            type: 'get',
+            url: url,
+            cache: false,
+            dataType: "jsonp", //跨域采用jsonp方式
+            processData: false,
+            timeout:10000, //超时时间，毫秒
+            complete: function (data) {
+                if (data.status==200) {
+                    $('#form').submit();
+                } else {
+                    alert("您输入的网址不存在，请重新输入！");
+                    return false;
+                }
+            }
+        });
+    });
+
+});
 function copy(){
     var e=document.getElementById("shortURL");//对象是content
     e.select(); //选择对象
     document.execCommand("Copy"); //执行浏览器复制命令
     alert("复制成功！");
-}
-function getURL() {
-    $("#msg").html("");
-    var url = $("#longURL").val();//请求的url
-    var dateTime = disptime();
-    var time2 = dateTime.DateTime;
-    $("#msg1").html("发送时间：" + time2);
-    $.ajax({
-        type: 'get',
-        url: url,
-        cache: false,
-        dataType: "jsonp", //跨域采用jsonp方式
-        processData: false,
-        timeout:10000, //超时时间，毫秒
-        complete: function (data) {
-            var dateTime2 = disptime();
-            var time22 = dateTime2.DateTime;
-            var htmlTxt =[];
-            if (data.status==200) {
-                htmlTxt.push("成功<br/>");
-            } else {
-                htmlTxt.push("失败<br/>");
-            }
-            htmlTxt.push("readyState=" + data.readyState + "<br/>status=" + data.status + "<br/>statusText=" + data.statusText + "<br/>响应时间：" + time22);
-            var htmlString = htmlTxt.join('');
-            $("#msg").html(htmlString);
-        }
-    });
-}
-function disptime() {
-    var date = new Date();
-    var yyyy = date.getFullYear();//四位年份
-    var month = date.getMonth() + 1;//月份 0-11
-    var day = date.getDate();//日
-    var HH = date.getHours();//时
-    var minute = date.getMinutes();//分钟
-    var second = date.getSeconds();//秒
-    var milliseconds=date.getMilliseconds();//毫秒
-    if (month < 10) {
-        month = "0" + month;
-    }
-    if (day < 10) {
-        day = "0" + day;
-    }
-    if (HH < 10) {
-        HH = "0" + HH;
-    }
-    if (minute < 10) {
-        minute = "0" + minute;
-    }
-    if (second < 10) {
-        second = "0" + second;
-    }
-    var time = yyyy + "-" + month + "-" + day + " " + HH + ":" + minute + ":" + second + " " + milliseconds;
-    var timeTxt = yyyy + month + day + HH + minute + second;
-    var time = {
-        DateTime: time,
-        TimeTxt: timeTxt
-    }
-    return time;
 }
